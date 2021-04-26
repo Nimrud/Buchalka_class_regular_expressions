e10efe9a38c99b6de0a721b91a845b0fb4fe5d80
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-04-26 23:37:10 +0200

    m02_quantifiers

---
+++ src/m02_quantifiers/Main.java
@@ -0,0 +1,36 @@
+package m02_quantifiers;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String aN = "abcDeeeKJyZhiij127ato7kz2";
+
+        // Quantifier określa, ile razy dany element może wystąpić w danym regular expression
+        // liczba w {} określa, ile razy poprzedni znak ma wystąpić:
+        System.out.println(aN.replaceAll("^abcDe{3}", "replace"));
+
+        // inna wersja, z + (plus mówi, że może to być jeden - lub więcej - dany znak)
+        // plus dotyczy tylko znaku poprzedzającego
+        System.out.println(aN.replaceAll("^abcDe+", "replace"));
+
+        // szukamy 0+ wystąpień znaku:
+        // gwiazdka - jak zwykle - dotyczy tylko znaku ją poprzedzającego
+        System.out.println(aN.replaceAll("^abcDe*", "replace"));
+        // przydatne np. gdy robimy walidację informacji podanych przez użytkownika,
+        // ale część z nich jest opcjonalna
+
+        // ograniczenie liczby wystąpień znaku do danego zakresu (np. od 2 do 5 znaków e):
+        System.out.println(aN.replaceAll("^abcDe{2,5}", "replace"));
+
+        // przykład zamiany:
+        // wszystkich wystąpień znaku h, za którym jest dowolna ilość i,
+        // przynajmniej jedno j - zamieniamy taki ciąg na _
+        System.out.println(aN.replaceAll("[h]i*j+", "_"));
+
+        // zamiana znaków a-z, oprócz j oraz k
+        System.out.println(aN.replaceAll("[a-z&&[^jk]]", "-"));
+
+        // PEŁNA DOKUMENTACJA:
+        // https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
+    }
+}
