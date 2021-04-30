947713d06218124c12f41f9974b5ca93204ae6d9
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-04-30 03:04:55 +0200

    m06_challenges2 (2)

--- src/m06_challenges2/Main.java
+++ src/m06_challenges2/Main.java
@@ -14,7 +14,7 @@ public class Main {
         String solution2a = "([a-z|A-Z]+)(\\.)(\\d+)";
         Pattern pattern2a = Pattern.compile(solution2a);
         Matcher matcher2a = pattern2a.matcher(challenge2a);
-        while (matcher2a.find()){
+        while (matcher2a.find()) {
             System.out.println("Number: " + matcher2a.group(3));
         }
 
@@ -24,11 +24,11 @@ public class Main {
         // The last match is followed by a new line
         // Revise the regular expression accordingly and extract all the numbers
         System.out.println("=== Challenge 2b ===");
-        String challenge2b ="abcd.135\tuvqz.7\ttzik.989\n";
+        String challenge2b = "abcd.135\tuvqz.7\ttzik.989\n";
         String solution2b = "[a-z|A-Z]+\\.(\\d+)[\\t|\\n]";
         Pattern pattern2b = Pattern.compile(solution2b);
         Matcher matcher2b = pattern2b.matcher(challenge2b);
-        while (matcher2b.find()){
+        while (matcher2b.find()) {
             System.out.println("Number: " + matcher2b.group(1));
         }
 
@@ -39,7 +39,7 @@ public class Main {
         // https://docs.oracle.com/javase/8/docs/api/java/util/regex/Matcher.html
         System.out.println("=== Challenge 2c ===");
         matcher2b.reset();
-        while (matcher2b.find()){
+        while (matcher2b.find()) {
             System.out.println("Start index: " + matcher2b.start(1)
                     + ", end index: " + (matcher2b.end(1) - 1));
             // dla end() odejmujemy 1, bo ta metoda zwraca indeks następujący po odnalezionej frazie
@@ -50,5 +50,30 @@ public class Main {
         // Extract what's in the curly braces
         System.out.println("=== Challenge 2d ===");
         String challenge2d = "{0, 2}, {0, 5}, {1, 3}, {2, 4}";
+        String solution2d = "\\{(\\d+, \\d+)\\}[, ]*";
+        // rozwiązanie Buchalki:
+        // String solution2d = "\\{(.+?)\\}";
+        Pattern pattern2d = Pattern.compile(solution2d);
+        Matcher matcher2d = pattern2d.matcher(challenge2d);
+        while (matcher2d.find()) {
+            System.out.println("Numbers: " + matcher2d.group(1));
+        }
+
+        // Challenge 2e
+        // Write a regular expression that will match a 5-digit US zip code
+        // Use '11111' as your test string
+        System.out.println("=== Challenge 2e ===");
+        String challenge2e = "11111";
+        String solution2e = "^\\d{5}$";
+        System.out.println(challenge2e.matches(solution2e));
+
+        // Challenge 2f
+        // US zip codes can be followed by a dash and another four numbers
+        // Write a regular expression that will match those zip codes
+        System.out.println("=== Challenge 2f ===");
+        String challenge2f = "11111-1111";
+        String solution2f = "^\\d{5}(-\\d{4})?$";
+        System.out.println(challenge2f.matches(solution2f));
+
     }
 }
